63e654c492299f0aa1cec78155554acabe96a421
Maheedhar_Windows_machine <Maheedhar@Windows>
2024-06-26 09:16:22 -0400

    upload jenkins files

---
+++ vars/docker.groovy
@@ -0,0 +1,37 @@
+def call() {
+    node {
+        git branch: 'main', url: "https://github.com/b57-clouddevops/${COMPONENT}.git"
+        common.lintchecks()
+        env.ARGS="-Dsonar.java.binaries=./target/"
+        // common.sonarchecks()
+        common.testcases()
+        if(env.TAG_NAME != null) {
+            stage('Generating & Publishing Artifacts') {
+            if(env.APPTYPE == "nodejs") {
+                    sh "echo Generating Node Artifacts"
+                    sh "npm install"               
+                }         
+            else if(env.APPTYPE == "python") {
+                    sh "echo Generating Artifacts"
+                    sh "zip -r ${COMPONENT}-${TAG_NAME}.zip *.py *.ini requirements.txt"        
+                }
+            else if(env.APPTYPE == "maven") {
+                    sh "mvn clean package"
+                    sh "mv target/${COMPONENT}-1.0.jar ${COMPONENT}.jar"
+                    sh "zip -r ${COMPONENT}-${TAG_NAME}.zip  ${COMPONENT}.jar"      
+                }
+            else if(env.APPTYPE == "angularjs") {
+                    sh "cd static/"
+                    sh "zip -r ../${COMPONENT}-${TAG_NAME}.zip *"
+                }
+            else { sh "echo Selected Component Type Doesnt Exist" }                        
+        }
+            sh "echo Downloading the pen key file for DB Connectivity"
+            sh "wget https://truststore.pki.rds.amazonaws.com/global/global-bundle.pem"
+            sh "echo Authenticating To ECR"
+            sh "aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 355449129696.dkr.ecr.us-east-1.amazonaws.com"
+            sh "docker build -t 355449129696.dkr.ecr.us-east-1.amazonaws.com/${COMPONENT}:${TAG_NAME} ."
+            sh "docker push 355449129696.dkr.ecr.us-east-1.amazonaws.com/${COMPONENT}:${TAG_NAME}"
+        }
+    }
+}
